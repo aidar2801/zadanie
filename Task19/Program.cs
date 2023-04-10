4d4587744b1688a74dc7fb951b5b5d8ea8a85d32
Your Name <you@example.com>
2023-04-10 22:03:30 +0500

    Task 19 написана

--- Task19/Program.cs
+++ Task19/Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 19
+// Напишите программу, которая принимает на вход пятизначное число и проверяет,
+// является ли оно палиндромом.
+Console.WriteLine("Введите пятизначное число");
+string number = Console.ReadLine();
+int length = number.Length;
+
+if (length == 5)
+{
+    if (number[0] == number[4] && number[1] == number[3])
+    {
+        Console.WriteLine($"{number} - Палиндром");
+    }
+    else
+    {
+        Console.WriteLine($"{number} - не палиндром");
+    }
+}
+else
+{
+    Console.WriteLine($"{number} - не является пятизначным");
+}
